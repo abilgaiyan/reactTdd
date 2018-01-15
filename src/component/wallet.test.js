30a130eb23a88c2325ea7d367f2c51183784ad53
ajay bilgaiyan <abilgaiyan@gmail.com>
2018-01-15 07:10:29 +0530

    first commit

---
+++ src/component/wallet.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import {Wallet} from './Wallet';
+
+describe('Wallet ', () => {
+    const props = { balance: 20 }; 
+    const wallet = shallow(<Wallet {...props} />);
+
+    it('Wallet component render properly',()=>{
+       
+        expect(wallet).toMatchSnapshot();
+
+    })
+    
+    it('display the balance from props', ()=>{
+        expect(wallet.find('.balance').text()).toEqual('Wallet balance: 20');
+    })
+});
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {render} from 'react-dom';
+import { createStore } from 'redux';
+import rootReducer from './reducers/balance';
+import { Provider } from 'react-redux';
+
+import App from './component/App';
+
+const store = createStore(rootReducer);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+   document.getElementById('root')
+);
+
